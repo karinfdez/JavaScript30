fa7eff658f9a8c3ce6850a9e1a0a0826f1c1f867
karinfdez <karinfdez@yahoo.com>
2018-11-11 21:42:50 -0500

    Update icon to show on play or pause when click on button

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,10 +1,17 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
-
+const toggle = document.querySelector('.toggle');
 
 function togglePlay() {
     const method = video.paused ? 'play' : 'pause';
     video[method]();
 }
 
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
