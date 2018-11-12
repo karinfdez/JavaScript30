cd4e3ade9ee35facb6ca694b2b71126b33d10755
karinfdez <karinfdez@yahoo.com>
2018-11-11 21:57:19 -0500

    Add logic to skip video back and force a few seconds

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,6 +1,7 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const toggle = document.querySelector('.toggle');
+const skipButtons = document.querySelectorAll('[data-skip]');
 
 function togglePlay() {
     const method = video.paused ? 'play' : 'pause';
@@ -12,6 +13,15 @@ function updateButton() {
     toggle.textContent = icon;
 }
 
+function skip(){
+    video.currentTime += parseFloat(this.dataset.skip);  //parsefloat to convert it to a real number
+}
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => {
+    button.addEventListener('click', skip);
+});
